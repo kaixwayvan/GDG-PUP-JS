59f072ef01009d20b6d847ba37b02fed7831723e
kaixwayvan <xanwayvan@gmail.com>
2025-02-15 21:33:18 +0800

    chore: complete activity on Conditional Statements

--- Assignment-Exercises/JS-3_Conditional-Statements/script.js
+++ Assignment-Exercises/JS-3_Conditional-Statements/script.js
@@ -1,3 +1,28 @@
 function checkAge() {
-    // write your code here 
+  // write your code here
+
+  let ageInput = document.getElementById("ageInput").value;
+  let printResult = document.querySelector("#result");
+  let category;
+
+  if (ageInput === "") {
+    printResult.innerHTML = "Invalid age. Please enter a valid number.";
+    return;
+  }
+
+  ageInput = parseInt(ageInput);
+
+  if (ageInput <= 0) {
+    category = "Invalid age. Please enter a valid number.";
+  } else {
+    if (ageInput <= 12) {
+      category = "Child";
+    } else if (ageInput <= 19) {
+      category = "Teenager";
+    } else {
+      category = "Adult";
+    }
+  }
+
+  printResult.innerHTML = `You are a(n) ${category}.`;
 }
