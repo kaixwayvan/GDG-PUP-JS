dc1286383ae5cc440fdd979757503954034c1660
kaixwayvan <xanwayvan@gmail.com>
2025-02-16 10:54:17 +0800

    Implement loops for array iteration in script.js

---
+++ Assignment-Exercises/JS-5_Arrays-and-Loops/script.js
@@ -0,0 +1,11 @@
+const favoriteMovies = [
+  "Interstellar",
+  "Se7en",
+  "Inside Out",
+  "What Happened to Monday",
+  "The Truman Show",
+];
+
+for (let i = 0; i < favoriteMovies.length; i++) {
+  console.log(favoriteMovies[i]);
+}
