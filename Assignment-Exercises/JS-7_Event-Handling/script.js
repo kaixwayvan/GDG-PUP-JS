71a8ae48c02f3a4aa45aedcf11ecdf6e07e250f8
kaixwayvan <xanwayvan@gmail.com>
2025-02-16 10:55:32 +0800

    Implement event listeners in script.js

---
+++ Assignment-Exercises/JS-7_Event-Handling/script.js
@@ -0,0 +1,3 @@
+document.getElementById("colorButton").addEventListener("click", () => {
+  document.body.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+});
