d0285c1b617a8f915b345c91deb63f939140250d
kaixwayvan <xanwayvan@gmail.com>
2025-02-16 10:56:11 +0800

    Implement DOM manipulations in script.js

--- Assignment-Exercises/JS-8_DOM-Manipulation/JS8.js
+++ Assignment-Exercises/JS-8_DOM-Manipulation/JS8.js
@@ -1,1 +1,21 @@
 // your code starts here
+const subtractButton = document.querySelector("#button-subtract");
+const addButton = document.querySelector("#button-add");
+const counterText = document.querySelector("#counter-text");
+
+subtractButton.addEventListener("click", whenSubtract);
+addButton.addEventListener("click", whenAdd);
+
+let counterVal = 0;
+
+function whenSubtract() {
+  if (counterVal > 0) {
+    counterVal--;
+    counterText.textContent = counterVal;
+  }
+}
+
+function whenAdd() {
+  counterVal++;
+  counterText.textContent = counterVal;
+}
