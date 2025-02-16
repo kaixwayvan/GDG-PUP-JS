12c1395d832e730012a2444b8295195a62b99d62
kaixwayvan <xanwayvan@gmail.com>
2025-02-16 10:53:38 +0800

    Implement for and while loops in script.js

--- Assignment-Exercises/JS-4_For-and-While-Loops/script.js
+++ Assignment-Exercises/JS-4_For-and-While-Loops/script.js
@@ -1,5 +1,5 @@
-// scripts.js
-
+// script.js
+/*
 // Example Using a for loop to print numbers 1 to 10
 for (let i = 1; i <= 10; i++) {
   console.log(`Number: ${i}`);
@@ -17,4 +17,28 @@ while (index < hobbies.length) {
   // Log each hobby and see if it works in the console
 
   index++;
+}
+*/
+
+for (let i=1; i <=10; i++) {
+  console.log(`Number: ${i}`);
+}
+
+let hobbies = [
+  "Reading",
+  "Writing",
+  "Cycling",
+  "Hiking",
+  "Cooking",
+  "Drawing",
+  "Gaming",
+  "Gardening",
+  "Photography",
+  "Traveling"
+];
+
+let j = 0;
+while (j < hobbies.length) {
+  console.log(`Hobby ${j}: ${hobbies[j]}`);
+  j++;
 }
